3ff8f2dc9f0ffe0106c114b995b9080888d88b27
MrLkuch <kucheida.lucas@gmail.com>
2024-05-17 10:16:08 +0200

    feat(01-looping-a-triangle.js): complete kata

---
+++ src/01-looping-a-triangle.js
@@ -0,0 +1,25 @@
+// Looping a triangle
+
+// Write a loop that makes seven calls to console.log to output the following triangle:
+let hastag = ''
+for (let i = 1; i <= 7; i++) {
+    console.log(hastag += '#');
+}
+
+```js
+#
+##
+###
+####
+#####
+######
+#######
+```
+
+    // It may be useful to know that you can find the length of a string by writing .length after it.
+
+    ```js
+let abc = "abc";
+console.log(abc.length);
+// → 3
+```
