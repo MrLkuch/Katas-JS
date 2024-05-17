e2aaeecdd28d2cc336d310d1a18d3b4af2c19b1b
MrLkuch <kucheida.lucas@gmail.com>
2024-05-17 12:44:51 +0200

    feat(07-sum-of-range.js): complete kata

---
+++ src/07-sum-of-range.js
@@ -0,0 +1,37 @@
+// The sum of a range
+
+function range(start, end, step = 1) {
+    let array = []
+    if (start > end) {
+        for (let i = start; i >= end; i += step) {
+            array.push(i);
+        }
+    }
+    else {
+
+        for (let i = start; i <= end; i += step) {
+            array.push(i);
+        }
+    }
+    return array;
+}
+
+function sum(array) {
+    let result = 0;
+    for (numbers in array) {
+        result += +array[numbers];
+    }
+    return result;
+}
+
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(range(1, 10, 2));
+
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+
+console.log(sum(range(1, 10)));
+// → 55
+
