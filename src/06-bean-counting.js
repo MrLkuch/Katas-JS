7e96b9c712aae95442e48f4c7cafe2084c7b4b6f
MrLkuch <kucheida.lucas@gmail.com>
2024-05-17 12:17:23 +0200

    feat(06-bean-counting.js): complete kata

---
+++ src/06-bean-counting.js
@@ -0,0 +1,28 @@
+// Bean Counting
+
+function countBs(string) {
+    let count = 0;
+    for (B in string) {
+        if (string[B] == 'B') {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+function countChar(string, letter) {
+    let count = 0;
+    for (letters in string) {
+        if (string[letters] == letter) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+console.log(countBs("BBC"));
+// → 2
+
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
