879880701784191e023b94f1279cd593ebeb5abc
MrLkuch <kucheida.lucas@gmail.com>
2024-05-21 11:08:17 +0200

    fix(Convert boolean values to string Yes or No.js): fix typo

---
+++ src/codewars/Convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,16 @@
+/* Complete the method that takes a boolean value and return a "Yes" string for true,
+or a "No" string for false.*/
+
+function boolToWord(bool) {
+    if (bool) {
+        return "Yes";
+
+    }
+    return "No"
+}
+
+// Other solution
+
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+}
