8b917d27fc13fb2fee4fc0b2bdbae30d84b2b897
MrLkuch <kucheida.lucas@gmail.com>
2024-05-21 15:53:18 +0200

    feat(Is he gonna survive.js): complete kata

---
+++ src/codewars/Is he gonna survive.js
@@ -0,0 +1,20 @@
+/*A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)*/
+
+function hero(bullets, dragons) {
+
+    while (dragons > 0) {
+        bullets -= 2;
+        dragons--;
+    }
+    if (dragons <= 0 && bullets >= 0) {
+        return true;
+    } return false;
+}
+
+// other solution
+
+function hero(bullets, dragons) {
+    return bullets >= dragons * 2
+}
